883baa5884ad93470168596a6df444e294791b6d
Jatin Mehta <mehta.r.jatin@gmail.com>
2023-12-27 19:14:54 -0500

    121

---
+++ 121.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        return self.min_max(prices)
+
+    def naive(self, prices):
+        profit = 0
+        for i in range(len(prices)):
+            for j in range(i + 1, len(prices)):
+                if prices[j] - prices[i] > profit:
+                    profit = prices[j] - prices[i]
+        return profit
+    
+    def max_price(self, prices):
+        profit = 0
+
+        # preprocessing
+        current_max = prices[-1]
+        max_arr = []
+        for i in range(1, len(prices) + 1):
+            if prices[-i] > current_max:
+                current_max = prices[-i]
+            max_arr.append(current_max)
+        max_arr.reverse()
+        
+        for i in range(len(prices)):
+            if max_arr[i] - prices[i] > profit:
+                profit = max_arr[i] - prices[i]
+        return profit
+
